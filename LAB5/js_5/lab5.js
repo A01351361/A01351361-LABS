c8850438124872ab9bf43d3f9fd1e8c8216f3da1
Erick Gerardo Calderon Reyes <A01351361@itesm.mx>
2021-02-14 18:50:05 -0600

    Tienda Funcional

--- LAB5/js_5/lab5.js
+++ LAB5/js_5/lab5.js
@@ -22,4 +22,22 @@ function verificar(){
 }
 
 
-//<button id = "Verificar" class = "button" onclick="verificar()"> Verificar </button>
+var Hamburguesas = document.getElementById("Hamburguesa");
+var Boneless = document.getElementById("Boneless");
+var Alitas = document.getElementById("Alitas");
+var Malteada = document.getElementById("Malteada");
+
+function Pagar(){
+    let suma = document.getElementById("costo");
+    costo_hamburguesa = Hamburguesas.value * 15;
+    costo_boneless = Boneless.value * 15;
+    costo_alitas = Alitas.value * 10;
+    costo_malteada = Malteada.value * 7;
+
+    sumatotal = costo_hamburguesa + costo_boneless + costo_alitas + costo_malteada;
+    iva = sumatotal * 0.16;
+    total_sin_iva = sumatotal-iva;
+
+    suma.innerHTML = "<p> El costo de los productos es de: $" + total_sin_iva + "</p>"+ "<p> El iva de 16% es de: $" + iva + "</p>" + "<p><strong>El costo total de su pedido a pagar es de: $" + sumatotal +"</strong></p>";
+}
+
